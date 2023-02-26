dc6649e7256af24935ed7e894030158a9dec5d87
iakmds <111225446+iakmds@users.noreply.github.com>
2023-02-26 21:00:16 +0100

    error handling: Show snackbar error if terminal command fails

--- lib/src/utils/terminal_command.dart
+++ lib/src/utils/terminal_command.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:buhocms/src/widgets/snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:process_run/shell_run.dart';
 
@@ -11,18 +12,21 @@ Future<void> runTerminalCommand({
   final shell = Shell(workingDirectory: workingDirectory);
 
   // Try to run command
-  await shell.run(command).then((value) {
-    // Command found, run it
-  }).catchError((object) async {
+  try {
+    await shell.run(command);
+  } catch (e) {
     // If error, check for Linux Flatpak sandbox issue
     if (Platform.isLinux) {
-      await shell.run('flatpak-spawn --host $command').then((value) {
-        // If platform is Flatpak and no error, command working
-      }).catchError((object) {
+      try {
+        await shell.run('flatpak-spawn --host $command');
+      } catch (_) {
         // Finally, if platform is Flatpak but still error, command not working
-      });
+        showSnackbar(text: e.toString(), seconds: 10);
+      }
+    } else {
+      showSnackbar(text: e.toString(), seconds: 10);
     }
-  });
+  }
 
   shell.kill();
 }
