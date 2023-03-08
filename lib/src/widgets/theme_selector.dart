875c5a8ed18de3bb033cdf73d8b121c25a1bada2
iakmds <111225446+iakmds@users.noreply.github.com>
2023-03-08 11:03:24 +0100

    ux(ThemeSelector): Reduce theme amount, set default theme

--- lib/src/widgets/theme_selector.dart
+++ lib/src/widgets/theme_selector.dart
@@ -26,6 +26,24 @@ class _ThemeSelectorState extends State<ThemeSelector> {
   void initState() {
     super.initState();
     schemeIndex = Preferences.getColorSchemeIndex();
+    if (schemeIndex != 27 &&
+        schemeIndex != 11 &&
+        schemeIndex != 8 &&
+        schemeIndex != 9 &&
+        schemeIndex != 4 &&
+        schemeIndex != 29 &&
+        schemeIndex != 14 &&
+        schemeIndex != 23 &&
+        schemeIndex != 33 &&
+        schemeIndex != 6) {
+      const defaultTheme = 27;
+
+      Preferences.setColorSchemeIndex(defaultTheme);
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        Provider.of<ThemeProvider>(context, listen: false)
+            .setColorScheme(defaultTheme);
+      });
+    }
 
     scrollController = ScrollController(
       initialScrollOffset: _kWidthOfScrollItem * schemeIndex,
@@ -38,10 +56,63 @@ class _ThemeSelectorState extends State<ThemeSelector> {
     super.dispose();
   }
 
+  List<FlexSchemeData> schemesList() {
+    return [
+      FlexColor.schemesList[27],
+      FlexColor.schemesList[11],
+      FlexColor.schemesList[8],
+      FlexColor.schemesList[9],
+      FlexColor.schemesList[4],
+      FlexColor.schemesList[29],
+      FlexColor.schemesList[14],
+      FlexColor.schemesList[23],
+      FlexColor.schemesList[33],
+      FlexColor.schemesList[6],
+    ];
+  }
+
+  List<Widget> colorWidgets() {
+    final list = <Widget>[];
+
+    for (var index = 0; index < schemesList().length; index++) {
+      var realIndex = 0;
+      for (var i = 0; i < FlexColor.schemesList.length; i++) {
+        if (FlexColor.schemesList[i].name == schemesList()[index].name) {
+          realIndex = i;
+        }
+      }
+
+      list.add(
+        Tooltip(
+          message: schemesList()[index].name,
+          child: FlexThemeModeOptionButton(
+            optionButtonBorderRadius: 12,
+            height: Preferences.getColorSchemeIndex() == realIndex ? 26 : 20,
+            width: Preferences.getColorSchemeIndex() == realIndex ? 26 : 20,
+            padding: const EdgeInsets.all(0.4),
+            optionButtonMargin: EdgeInsets.zero,
+            borderRadius: 0,
+            unselectedBorder: BorderSide.none,
+            selectedBorder: BorderSide(
+                color: Theme.of(context).primaryColorLight, width: 5),
+            onSelect: () {
+              setState(() => schemeIndex = index);
+              Provider.of<ThemeProvider>(context, listen: false)
+                  .setColorScheme(realIndex);
+            },
+            selected: Preferences.getColorSchemeIndex() == realIndex,
+            backgroundColor: Theme.of(context).colorScheme.surface,
+            flexSchemeColor: schemesList()[index].light,
+          ),
+        ),
+      );
+    }
+
+    return list;
+  }
+
   @override
   Widget build(BuildContext context) {
-    bool isLight =
-        Preferences.getThemeMode() == ThemeMode.dark.name ? false : true;
     return ScrollConfiguration(
       behavior: ScrollConfiguration.of(context).copyWith(
         dragDevices: {
@@ -53,37 +124,8 @@ class _ThemeSelectorState extends State<ThemeSelector> {
         controller: scrollController,
         child: SingleChildScrollView(
           controller: scrollController,
-          scrollDirection: Axis
-              .horizontal, //also see https://github.com/flutter/flutter/issues/75180
-          child: Row(
-            children: [
-              for (var index = 0; index < FlexColor.schemesList.length; index++)
-                FlexThemeModeOptionButton(
-                  optionButtonBorderRadius: 12,
-                  height: Preferences.getColorSchemeIndex() == index ? 26 : 20,
-                  width: Preferences.getColorSchemeIndex() == index ? 26 : 20,
-                  padding: const EdgeInsets.all(0.4),
-                  optionButtonMargin: EdgeInsets.zero,
-                  borderRadius: 0,
-                  unselectedBorder: BorderSide.none,
-                  selectedBorder: BorderSide(
-                    color: Theme.of(context).primaryColorLight,
-                    width: 5,
-                  ),
-                  onSelect: () {
-                    setState(() {
-                      schemeIndex = index;
-                    });
-                    var themeProvider =
-                        Provider.of<ThemeProvider>(context, listen: false);
-                    themeProvider.setColorScheme(index);
-                  },
-                  selected: Preferences.getColorSchemeIndex() == index,
-                  backgroundColor: Theme.of(context).colorScheme.surface,
-                  flexSchemeColor: FlexColor.schemesList[index].light,
-                )
-            ],
-          ),
+          scrollDirection: Axis.horizontal,
+          child: Row(children: colorWidgets()),
         ),
       ),
     );
